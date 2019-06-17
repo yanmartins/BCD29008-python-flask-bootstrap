c03df765417b959a38b5c0e54e8cb1f81b4a3cd1
Yan Martins <yanlm1998@gmail.com>
2019-06-17 16:44:37 -0300

    Exemplo 2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, render_template, flash, redirect, url_for, session
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from LoginForm import LoginForm
@@ -17,11 +17,12 @@ db = SQLAlchemy(app)
 
 class Usuario(db.Model):
     __tablename__ = 'Usuario'
-    username = db.column(db.String(40), primary_key=True)
-    password = db.column(db.String(40))
+
+    username = db.Column(db.String(40), primary_key=True)
+    password = db.Column(db.String(40))
 
     def __init__(self, **kwargs):
-        super().__init__(kwargs)
+        super().__init__(**kwargs)
         self.username = kwargs.pop('username')
         self.password = generate_password_hash(kwargs.pop('password'))
 
@@ -32,14 +33,40 @@ class Usuario(db.Model):
         return check_password_hash(self.password, password)
 
 
+@app.route('/painel')
+def mostrar_painel():
+
+    if session.get('autenticado') == True:
+        return render_template('painel.html', titulo='Área pessoal')
+
+    return redirect(url_for('autenticar_usuario'))
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def autenticar_usuario():
     formulario = LoginForm()
     if formulario.validate_on_submit():
-        render_template('index.html')
+
+        user = formulario.username.data
+        senha = formulario.password.data
+
+        u = Usuario.query.filter_by(username=user).first()
+
+        if u is not None:
+            if u.check_password(senha):
+
+                session['username'] = user
+                session['autenticado'] = True
+
+                return redirect(url_for('mostrar_painel'))
 
     return render_template('login.html', titulo="Autenticar",form=formulario)
+
+
+@app.route('/logout')
+def deslogar_usuario():
+    session['autenticado'] = False
+    return redirect(url_for('autenticar_usuario'))
 
 @app.route('/')
 def inicio():
@@ -47,4 +74,5 @@ def inicio():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    db.create_all()
+    #app.run(debug=True, host='0.0.0.0')
